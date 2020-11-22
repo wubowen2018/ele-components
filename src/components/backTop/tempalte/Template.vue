4eee5f6fa66fd67700fc598a0b471423f19f91fb
wubowen2018 <wubowen2017@gmail.com>
2020-11-23 00:01:54 +0800

    添加Vue模版文件

---
+++ src/components/backTop/tempalte/Template.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        This is a Vue template page 
+    </div>
+</template>
+
+<script>
+export default {
+    name: "",
+    data() {
+        return {
+
+        }
+    },
+    props: {
+
+    },
+    methods: {
+
+    },
+    mounted() {
+        
+    }
+}
+</script>
+
+<style lang="less" scoped>
+   
+</style>
